79324fd3a6851ef0b11d7a235cb56131da4c7af5
Askar <nurbekovaskar099@gmail.com>
2026-03-11 20:05:17 +0600

    Starting files

---
+++ src/main/java/kg/attractor/jobsearchjava27/JobSearchJava27Application.java
@@ -0,0 +1,13 @@
+package kg.attractor.jobsearchjava27;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSearchJava27Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobSearchJava27Application.class, args);
+    }
+
+}
---
+++ src/test/java/kg/attractor/jobsearchjava27/JobSearchJava27ApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.attractor.jobsearchjava27;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSearchJava27ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
