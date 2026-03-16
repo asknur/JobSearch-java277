326e2bb207f944a53e6bce891bc64e5cf83f0997
Askar <nurbekovaskar099@gmail.com>
2026-03-16 17:41:28 +0600

    Added endpoint for profile image

--- src/main/java/kg/attractor/jobsearchjava27/controller/AvatarController.java
+++ src/main/java/kg/attractor/jobsearchjava27/controller/AvatarController.java
@@ -1,5 +1,28 @@
 package kg.attractor.jobsearchjava27.controller;
 
-public class AvatarController {
+import kg.attractor.jobsearchjava27.dto.ImageDto;
+import kg.attractor.jobsearchjava27.service.ImageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+
+@Configuration
+@RequestMapping("images")
+@RequiredArgsConstructor
+public class AvatarController {
+    private final ImageService imageService;
+
+    @GetMapping
+    public ResponseEntity<?> getImage(@RequestParam(name = "filename")String filename){
+        return imageService.getById(filename);
+    }
+
+    @PostMapping
+    public HttpStatus create(@RequestBody ImageDto imageDto){
+        imageService.create(imageDto);
+        return HttpStatus.CREATED;
+    }
 }
