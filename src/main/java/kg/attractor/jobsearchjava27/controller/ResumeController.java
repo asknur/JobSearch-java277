cdaa4b1b98d722196fe60c778a2ff9cc216d05c9
Askar <nurbekovaskar099@gmail.com>
2026-03-16 17:42:47 +0600

    Added endpoint for resume

--- src/main/java/kg/attractor/jobsearchjava27/controller/ResumeController.java
+++ src/main/java/kg/attractor/jobsearchjava27/controller/ResumeController.java
@@ -1,13 +1,14 @@
 package kg.attractor.jobsearchjava27.controller;
 
 import kg.attractor.jobsearchjava27.model.Resume;
+import kg.attractor.jobsearchjava27.model.Vacancy;
 import kg.attractor.jobsearchjava27.service.ResumeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/resumes")
@@ -15,7 +16,31 @@ import org.springframework.web.bind.annotation.RestController;
 public class ResumeController {
     private final ResumeService resumeService;
 
+    @PostMapping
     public ResponseEntity<Resume> createResume(@RequestBody Resume resume) {
         return new ResponseEntity<>(resumeService.save(resume), HttpStatus.OK);
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Resume> updateResume(@PathVariable int id, @RequestBody Resume resume) {
+        resume.setId(id);
+        return new ResponseEntity<>(resumeService.save(resume), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteResume(@PathVariable int id) {
+        resumeService.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Resume>> getAllResumes() {
+        return new ResponseEntity<>(resumeService.getAllResume(), HttpStatus.OK);
+    }
+
+    @GetMapping("/resume/{id}")
+    public ResponseEntity<List<Resume>> getResumesByCategory(@PathVariable int id) {
+        return new ResponseEntity<>(resumeService.getResumeByCategory(id), HttpStatus.OK);
+    }
+
 }
