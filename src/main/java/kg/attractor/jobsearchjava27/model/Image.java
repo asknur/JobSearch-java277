507cf50c697dc31119505441d43525c3886a4809
Askar <nurbekovaskar099@gmail.com>
2026-03-22 16:42:41 +0600

    Created image model

---
+++ src/main/java/kg/attractor/jobsearchjava27/model/Image.java
@@ -0,0 +1,13 @@
+package kg.attractor.jobsearchjava27.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Image {
+    private Long id;
+    private Long userId;
+    private String fileName;
+
+}
