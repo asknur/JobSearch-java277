51ff46bf77d91bbd274a3d2f93d9c3008138297b
Askar <nurbekovaskar099@gmail.com>
2026-03-30 18:53:05 +0600

    fixed endpoints

--- src/main/java/kg/attractor/jobsearchjava27/controller/VacancyResponseController.java
+++ src/main/java/kg/attractor/jobsearchjava27/controller/VacancyResponseController.java
@@ -1,22 +1,28 @@
 package kg.attractor.jobsearchjava27.controller;
 
+import kg.attractor.jobsearchjava27.model.User;
+import kg.attractor.jobsearchjava27.model.Vacancy;
+import kg.attractor.jobsearchjava27.service.VacancyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/response/vacancies")
 @RequiredArgsConstructor
 public class VacancyResponseController {
+    private final VacancyService vacancyService;
 
     @PostMapping("/{id}/apply")
-    public ResponseEntity<String> respondVacancy(@PathVariable int id) {
-        return new ResponseEntity<>("Отклик на вакансию " + id + " сохранён", HttpStatus.OK);
+    public ResponseEntity<List<Vacancy>> respondVacancy(@PathVariable int id, @RequestParam int userId) {
+        return new ResponseEntity<>(vacancyService.getRespondedVacancies(userId), HttpStatus.OK);
     }
 
     @GetMapping("/{id}/applicants")
-    public ResponseEntity<String> getApplicants(@PathVariable int id) {
-        return new ResponseEntity<>("Список соискателей на вакансию " + id, HttpStatus.OK);
+    public ResponseEntity<List<User>> getApplicants(@PathVariable int id) {
+        return new ResponseEntity<>(vacancyService.getApplicantsByVacancyId(id), HttpStatus.OK);
     }
 }
