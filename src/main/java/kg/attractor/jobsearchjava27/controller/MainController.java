93239747ffde482e81f2caebdd322c4542738d8b
Askar <nurbekovaskar099@gmail.com>
2026-04-09 18:51:41 +0600

    Added vacancy list to controller

--- src/main/java/kg/attractor/jobsearchjava27/controller/MainController.java
+++ src/main/java/kg/attractor/jobsearchjava27/controller/MainController.java
@@ -1,15 +1,23 @@
 package kg.attractor.jobsearchjava27.controller;
 
+import kg.attractor.jobsearchjava27.service.VacancyService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
+@RequiredArgsConstructor
 public class MainController {
+    private final VacancyService vacancyService;
 
     @GetMapping
-    public String index(Model model) {
-        model.addAttribute("world", "World");
-        return "index";
+    public String showInfo(Model model) {
+        model.addAttribute("vacancy", vacancyService.getAllVacancies());
+        return "vacancy";
     }
+
+
 }
