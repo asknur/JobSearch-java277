140a41f3bfd44811f30059d873d564bc12e0ccef
Askar <nurbekovaskar099@gmail.com>
2026-04-09 18:52:38 +0600

    Fixes

--- src/main/java/kg/attractor/jobsearchjava27/model/Vacancy.java
+++ src/main/java/kg/attractor/jobsearchjava27/model/Vacancy.java
@@ -1,20 +1,20 @@
 package kg.attractor.jobsearchjava27.model;
 
-import lombok.Builder;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.security.Timestamp;
+import java.time.LocalDateTime;
 
 @Data
 @Builder
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Vacancy {
     private Long id;
-    private Timestamp updateTime;
-    private Timestamp createdTime;
+    private LocalDateTime updateTime;
+    private LocalDateTime createdTime;
     private Long authorId;
     private boolean isActive;
     private Integer expTo;
