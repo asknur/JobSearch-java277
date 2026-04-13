7b7eba6947c6e9a9ad6b7327bd0c26d880cd2ba3
Askar <nurbekovaskar099@gmail.com>
2026-04-13 18:50:00 +0600

    updated exceptions

--- src/main/java/kg/attractor/jobsearchjava27/exception/ImageNotFoundException.java
+++ src/main/java/kg/attractor/jobsearchjava27/exception/ImageNotFoundException.java
@@ -2,7 +2,7 @@ package kg.attractor.jobsearchjava27.exception;
 
 import java.nio.file.NoSuchFileException;
 
-public class ImageNotFoundException extends NoSuchFileException {
+public class ImageNotFoundException extends NotFoundEntryException {
     public ImageNotFoundException() {
         super("Image not found");
     }
--- src/main/java/kg/attractor/jobsearchjava27/exception/ResumeNotFoundException.java
+++ src/main/java/kg/attractor/jobsearchjava27/exception/ResumeNotFoundException.java
@@ -2,7 +2,7 @@ package kg.attractor.jobsearchjava27.exception;
 
 import java.nio.file.NoSuchFileException;
 
-public class ResumeNotFoundException extends NoSuchFileException {
+public class ResumeNotFoundException extends NotFoundEntryException {
   public ResumeNotFoundException() {
     super("Resume not found: ");
   }
