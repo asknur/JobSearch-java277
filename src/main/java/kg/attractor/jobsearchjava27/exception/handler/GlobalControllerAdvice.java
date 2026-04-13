74f0f241375f66905aed2e33e25b90f05c131f42
Askar <nurbekovaskar099@gmail.com>
2026-04-13 18:51:53 +0600

    updated file for new error template

--- src/main/java/kg/attractor/jobsearchjava27/exception/handler/GlobalControllerAdvice.java
+++ src/main/java/kg/attractor/jobsearchjava27/exception/handler/GlobalControllerAdvice.java
@@ -1,34 +1,52 @@
 package kg.attractor.jobsearchjava27.exception.handler;
 
+import jakarta.servlet.http.HttpServletRequest;
 import kg.attractor.jobsearchjava27.exception.NotFoundEntryException;
 import kg.attractor.jobsearchjava27.service.ErrorService;
 import lombok.RequiredArgsConstructor;
+import org.h2.engine.Mode;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.sql.SQLException;
+import java.util.stream.Collectors;
 
-@RestControllerAdvice
+@ControllerAdvice
 @RequiredArgsConstructor
 public class GlobalControllerAdvice {
     private final ErrorService errorService;
 
-
     @ExceptionHandler(NotFoundEntryException.class)
-    private ResponseEntity<ErrorResponseBody> noSuchFileExceptionHandler(NotFoundEntryException e) {
-        return new ResponseEntity<>(errorService.makeResponse(e, e.getClass().getSimpleName()), HttpStatus.NOT_FOUND);
+    private String noSuchFileExceptionHandler(HttpServletRequest request , Model model, NotFoundEntryException e) {
+        model.addAttribute("status", HttpStatus.NOT_FOUND.value());
+        model.addAttribute("reason", HttpStatus.NOT_FOUND.getReasonPhrase() + ": " + e.getMessage());
+        model.addAttribute("details", request);
+        return "errors/error";
     }
 
     @ExceptionHandler(SQLException.class)
-    private ResponseEntity<ErrorResponseBody> sqlExceptionHandler(SQLException e) {
-        return new ResponseEntity<>(errorService.makeResponse(e, e.getClass().getSimpleName()), HttpStatus.BAD_REQUEST);
+    private String sqlExceptionHandler(HttpServletRequest request ,Model model, SQLException e) {
+        model.addAttribute("status", HttpStatus.BAD_REQUEST.value());
+        model.addAttribute("reason", HttpStatus.BAD_REQUEST.getReasonPhrase() + ": " + e.getMessage());
+        model.addAttribute("details", request);
+        return "errors/error";
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    private ResponseEntity<ErrorResponseBody> validationHandler(MethodArgumentNotValidException e) {
-        return new ResponseEntity<>(errorService.makeResponse(e.getBindingResult()), HttpStatus.BAD_REQUEST);
+    private String validationHandler(HttpServletRequest request ,Model model ,MethodArgumentNotValidException e) {
+        String errors = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(fe -> fe.getField() + ": " + fe.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+        model.addAttribute("status", HttpStatus.BAD_REQUEST.value());
+        model.addAttribute("reason", "Validation failed: " + errors);
+        model.addAttribute("details", request);
+        return "errors/error";
     }
 }
