5450daffc2c966ef2a8a15db261b0af00aa74327
Askar <nurbekovaskar099@gmail.com>
2026-04-15 22:20:51 +0600

    added new annotations

--- src/main/java/kg/attractor/jobsearchjava27/model/Image.java
+++ src/main/java/kg/attractor/jobsearchjava27/model/Image.java
@@ -1,10 +1,15 @@
 package kg.attractor.jobsearchjava27.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
 
+@Data
+@Builder
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Image {
     private Long id;
     private Long userId;
