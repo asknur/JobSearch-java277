5bf83ff48105dee69c9f2d5f0c35f06ad7a7bc63
Askar <nurbekovaskar099@gmail.com>
2026-04-15 22:19:40 +0600

    Added Hibernate annotations

--- src/main/java/kg/attractor/jobsearchjava27/model/EducationInfo.java
+++ src/main/java/kg/attractor/jobsearchjava27/model/EducationInfo.java
@@ -1,18 +1,40 @@
 package kg.attractor.jobsearchjava27.model;
 
-import lombok.Builder;
-import lombok.Data;
+import jakarta.persistence.*;
+import lombok.*;
+
 import java.time.LocalDate;
 
 @Data
 @Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "education_info", schema = "public")
 public class EducationInfo {
-    private Integer id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "degree")
     private String degree;
+
+    @Column(name = "start_date")
     private LocalDate startDate;
+
+    @Column(name = "end_date")
     private LocalDate endDate;
+
+    @Column(name = "program")
     private String program;
+
+    @Column(name = "institution")
     private String institution;
-    private Resume resumeId;
+
+    @ManyToOne
+    @JoinColumn(name = "resume_id")
+    private Resume resume;
 
 }
--- src/main/java/kg/attractor/jobsearchjava27/model/User.java
+++ src/main/java/kg/attractor/jobsearchjava27/model/User.java
@@ -1,6 +1,6 @@
 package kg.attractor.jobsearchjava27.model;
 
-import lombok.Builder;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,16 +8,37 @@ import lombok.Setter;
 @Data
 @Getter
 @Setter
+@Entity
+@Table(name = "usr", schema = "public")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "account_type")
     private String accountType;
+
+    @Column(name = "avatar")
     private String avatar;
+
+    @Column(name = "phone_number")
     private String phoneNumber;
-    private String text;
+
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "age")
     private Integer age;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "surname")
     private String surname;
+
+    @Column(name = "password")
     private String password;
 
+    @Column(name = "enabled")
+    private Boolean enabled;
 }
