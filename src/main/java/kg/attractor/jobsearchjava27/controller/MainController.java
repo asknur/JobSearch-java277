a245d0134a440848b00454f456c62accf04821a6
Askar <nurbekovaskar099@gmail.com>
2026-04-27 18:43:19 +0600

    fixed sorting

--- src/main/java/kg/attractor/jobsearchjava27/controller/MainController.java
+++ src/main/java/kg/attractor/jobsearchjava27/controller/MainController.java
@@ -19,11 +19,13 @@ public class MainController {
     @GetMapping("/")
     public String index(Model model,
                         @RequestParam(defaultValue = "0") int page,
-                        @RequestParam(defaultValue = "5") int size) {
-        Page<VacancyDto> vacancyPage = vacancyService.getActiveVacanciesPage(page, size);
+                        @RequestParam(defaultValue = "5") int count,
+                        @RequestParam(defaultValue = "newest") String sort) {
+        Page<VacancyDto> vacancyPage = vacancyService.getActiveVacanciesPage(page, count, sort);
         model.addAttribute("vacancy", vacancyPage.getContent());
         model.addAttribute("currentPage", page);
         model.addAttribute("totalPages", vacancyPage.getTotalPages());
+        model.addAttribute("sort", sort);
         return "index";
     }
 }
